9c64514e437953230ff115c4282e82e472438b0e
Jong sung Lee <jongsunglee@Jongui-MacBookAir.local>
2020-11-09 01:35:36 +0900

    add gogo

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello! react!
+          Hello! react!  gogo!!
         </a>
       </header>
     </div>
